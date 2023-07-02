409a92683f72732daf2f0da6744594d9bbad4230
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2023-07-02 19:58:12 +0300

    Add files via upload

---
+++ StudenInformationSystem/src/java101/Teacher.java
@@ -0,0 +1,15 @@
+package java101;
+
+public class Teacher {
+	String name;
+	String mpno;
+	String branch;
+
+	Teacher(String name, String mpno, String branch) {
+		this.name = name;
+		this.mpno = mpno;
+		this.branch = branch;
+
+	}
+
+}
